6c20e418e71170aee53e6ba70e9ae43322610f7d
tithyana <yanacurry@yahoo.com>
2025-05-05 18:50:23 +0700

    cell01 ex03 updated

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,11 @@
-first_name = input("Hey, what's your first name? : ").strip()
-last_name = input("And your last name? : ").strip()
-print("Well, pleased to meet you,", first_name, last_name + ".")
+# 1st solution 
+# first_name = input("Hey, what's your first name? : ").strip()
+# last_name = input("And your last name? : ").strip()
+# print("Well, pleased to meet you,", first_name, last_name + ".")
+
+# 2nd solution
+def name():
+    first_name = input("Hey, what's your first name? : ").strip()
+    last_name = input("And your last name? : ").strip()
+    print(f"Well, pleased to meet you, {first_name} {last_name}.")
+name()
