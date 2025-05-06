8120002f5321a9296a6458403fa8a5f692dc9be1
tithyana <yanacurry@yahoo.com>
2025-05-06 17:46:01 +0700

    age.py updated

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -3,4 +3,4 @@
 age = int(input("Please tell me your age: "))
 print(f"You are currently {age} years old.")
 for i in range(1,4):
-    print(f"In {i * 10} years, you'll be {age + i * 10}")
+    print(f"In {i * 10} years, you'll be {age + i * 10} years old.")
