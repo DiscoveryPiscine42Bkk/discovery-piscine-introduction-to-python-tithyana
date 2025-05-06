2923967e8fea85f653c58401e88110ecdbf024cf
tithyana <yanacurry@yahoo.com>
2025-05-06 20:25:32 +0700

    addvanced mult updated

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -2,18 +2,21 @@
 
 # this program will display all multiplication table
 # use two while loops
+import sys
 
-i = 0
-j = 0
-while (i < 11):
-    print(f"Table de {i}:", end= " ")
-    while (j < 11):
-        print(f"{i*j}", end= " ")
-        j += 1
+if (len(sys.argv) == 1):
+    i = 0
     j = 0
-    i += 1
-    print("")
-    
+    while (i < 11):
+        print(f"Table de {i}:", end= " ")
+        while (j < 11):
+            print(f"{i*j}", end= " ")
+            j += 1
+        j = 0
+        i += 1
+        print("")
+else:
+    print("none")
 
 
 
