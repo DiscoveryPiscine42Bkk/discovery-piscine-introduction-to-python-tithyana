5ed068886800a39a724d1d145f3a03598b77532c
tithyana <yanacurry@yahoo.com>
2025-05-06 16:03:44 +0700

    igotthat updated

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-# create a while loop with break
-# it breaks when the response input is "STOP"
 
-def response():
-    a = input("What you gotta say? : ")
-    while True:
-        b = input("I got that! Anything else? : ")
-        if b == "STOP":
-            break
-response()
+# 1
+# def response():
+#     a = input("What you gotta say? : ")
+#     while True:
+#         b = input("I got that! Anything else? : ")
+#         if b == "STOP":
+#             break
+# response()
+
+#2
+a = input("What you gotta say? : ")
+while a != "STOP":
+    a = input("I got that! Anything else? : ")
