0b28f66450172ac002bb03a3951aea9a3d5086d5
tithyana <yanacurry@yahoo.com>
2025-05-07 16:25:57 +0700

    cell05ex03 updated

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
+# solution 1
+# a = [2, 8, 9, 48, 8, 22, -12, 2]
+# c = set()
+# for i in range(len(a)):
+#     if a[i] > 5:
+#         c.add(a[i] + 2)
+# print(a)
+# print(c)
+
+# solution 2
 a = [2, 8, 9, 48, 8, 22, -12, 2]
-c = set()
+b = []
 for i in range(len(a)):
     if a[i] > 5:
-        c.add(a[i] + 2)
+        b.append(a[i] + 2)
+b = set(b)
 print(a)
-print(c)
+print(b)
