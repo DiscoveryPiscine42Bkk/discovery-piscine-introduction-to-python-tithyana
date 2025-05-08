fc9a48966ba9d171fb6ff8d745bfe6d0e456548f
tithyana <yanacurry@yahoo.com>
2025-05-08 11:47:17 +0700

    scan_it updated

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -5,8 +5,9 @@ import sys
 l = len(sys.argv)
 c = 0
 if l == 3:
-    a = sys.argv[1]
-    b = sys.argv[2]
+    a = sys.argv[1] #keyword
+    b = sys.argv[2] #string
+    #search for the keyword inside the string
     c = b.count(a)
     if c != 0:
         print(c)
