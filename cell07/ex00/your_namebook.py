ab851afca7ce25f5c985c2e8e4f75060f0942cba
tithyana <yanacurry@yahoo.com>
2025-05-09 12:39:38 +0700

    yr_namebook updated

--- cell07/ex00/your_namebook.py
+++ cell07/ex00/your_namebook.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def array_of_names(d):
+def array_of_names(d : dict) -> list:
     a = []
     for k, v in d.items():
         a.append(f"{k.capitalize()} {v.capitalize()}")
